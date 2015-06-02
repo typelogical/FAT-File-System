f4251e4aebe3dea970aca7b283594d130cddaae2
Typelogical <william_wu_ext@yhaoo.com>
2015-06-01 19:54:24 -0700

    Added table.h table.cpp

---
+++ table.h
@@ -0,0 +1,15 @@
+#ifndef TABLE
+#define TABLE
+#include "fileSystem.h"
+class Table : public FileSystem {
+public:
+	Table (string diskName, size_t blockSize, size_t numBlocks,
+		string flatFile, string indexFile);
+	int buildTable (string inputFile);
+	int search (string val);
+private:
+	string flatFile;
+	string indexFile;
+	int indexSearch (string val);
+};
+#endif
